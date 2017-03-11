dae276412cd8bd475049ff5ca008ce64469f6faa
Eben <me@ebenroux.co.za>
2017-03-11 11:35:08 +0200

    - package update

--- Shuttle.Core.Ninject.Tests/NinjectComponentContainerFixture.cs
+++ Shuttle.Core.Ninject.Tests/NinjectComponentContainerFixture.cs
@@ -34,5 +34,23 @@ namespace Shuttle.Core.Ninject.Tests
             RegisterCollection(container);
             ResolveCollection(container);
         }
-    }
+
+		[Test]
+		public void Should_be_able_to_register_and_resolve_a_multiple_singleton()
+		{
+			var container = new NinjectComponentContainer(new StandardKernel());
+
+			RegisterMultipleSingleton(container);
+			ResolveMultipleSingleton(container);
+		}
+
+		[Test]
+		public void Should_be_able_to_register_and_resolve_multiple_transient_components()
+		{
+			var container = new NinjectComponentContainer(new StandardKernel());
+
+			RegisterMultipleTransient(container);
+			ResolveMultipleTransient(container);
+		}
+	}
 }
