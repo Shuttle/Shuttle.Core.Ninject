dbda65259d935f04434bad92a46bd31f51a8ad92
Eben <me@ebenroux.co.za>
2022-02-06 11:24:02 +0200

    - package upgrade

--- Shuttle.Core.Ninject/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Ninject/Properties/AssemblyInfo.cs
@@ -33,10 +33,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard 2.0")]
 #endif
 
-[assembly: AssemblyVersion("10.0.7.0")]
+[assembly: AssemblyVersion("10.1.0.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2019")]
 [assembly: AssemblyProduct("Shuttle.Core.Ninject")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.7")]
+[assembly: AssemblyInformationalVersion("10.1.0")]
 [assembly: ComVisible(false)]
